b57d0feee2bf698ad0beb58590adbf0874d52162
nahin <nurealamnahin16@gmail.com>
2026-07-23 13:24:02 +0600

    admin interface done

---
+++ Library_Management_System/Repository/IRepositroy/IAdminRepository.cs
@@ -0,0 +1,20 @@
+﻿using Library_Management_System.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Management_System.Repository.IRepositroy
+{
+    public interface IAdminRepository
+    {
+        void Add(Admin admin);
+
+        void Update(int Id);
+
+        void Delete(int Id);
+
+        List<Admin> GetAll();
+    }
+}
